03a516b9e2dd39285340d12a5bc0d64d22adb5f1
saulbuilds <larryklosowski@proton.me>
2025-10-12 12:36:45 -0700

    added ci/cd for deployment and publishing of assets

---
+++ lattice-v3/sdk/javascript/rollup.config.js
@@ -0,0 +1,34 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/index.esm.js',
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve({
+        preferBuiltins: true,
+      }),
+      commonjs(),
+      typescript({
+        tsconfig: './tsconfig.json',
+        declaration: true,
+        declarationDir: 'dist',
+        rootDir: 'src',
+      }),
+    ],
+    external: ['axios', 'ethers', 'events', 'ws', 'crypto', 'fs', 'path'],
+  },
+];
